ec0a848546d9e4f9e20ef6df3156cceaa30bae02
Roman Savenko <rsaven.wk@gmail.com>
2024-09-17 16:38:48 +0300

    feat: update migrations

--- SimpleUserManager/Migrations/20240916101728_add_user_email_unique_index.Designer.cs
+++ SimpleUserManager/Migrations/20240916101728_add_user_email_unique_index.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SimpleUserManager.DataContext;
 
@@ -11,9 +12,11 @@ using SimpleUserManager.DataContext;
 namespace SimpleUserManager.Migrations
 {
     [DbContext(typeof(UserManagerDbContext))]
-    partial class UserManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240916101728_add_user_email_unique_index")]
+    partial class add_user_email_unique_index
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
