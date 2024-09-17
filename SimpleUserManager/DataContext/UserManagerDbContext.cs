4e5cc5cab722c4b3d87967b95ba3b4de8a7833d3
Roman Savenko <rsaven.wk@gmail.com>
2024-09-17 16:30:42 +0300

    feat: add unique index for email user field

--- SimpleUserManager/DataContext/UserManagerDbContext.cs
+++ SimpleUserManager/DataContext/UserManagerDbContext.cs
@@ -6,4 +6,9 @@ namespace SimpleUserManager.DataContext;
 public class UserManagerDbContext(DbContextOptions<UserManagerDbContext> options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+    }
 }
