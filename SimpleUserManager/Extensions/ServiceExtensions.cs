4dec3a975dc4358bb7ca4147f3be2a2b00ac20eb
Roman Savenko <rsaven.wk@gmail.com>
2024-09-17 16:38:35 +0300

    feat: update pipeline

--- SimpleUserManager/Extensions/ServiceExtensions.cs
+++ SimpleUserManager/Extensions/ServiceExtensions.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
 using SimpleUserManager.DataContext;
+using SimpleUserManager.Repositories;
+using SimpleUserManager.Services;
+using SimpleUserManager.Services.Helpers;
+using System.Reflection;
 
 namespace SimpleUserManager.Extensions;
 
@@ -17,5 +22,34 @@ public static class ServiceExtensions
         {
             options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 36)));
         });
+    }
+
+    public static void AddAutoMapper(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddAutoMapper(Assembly.GetExecutingAssembly());
+    }
+
+    public static void ConfigureAuthentication(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+                options.Cookie.Name = "TastyCookie";
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+                options.LoginPath = "/Register";
+            });
+    }
+
+    public static void AddRepositories(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddScoped<IUserRepository, UserRepository>();
+    }
+
+    public static void AddServices(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddScoped<IPasswordHasher, PasswordHasher>();
+        serviceCollection.AddScoped<IPrincipalProvider, PrincipalProvider>();
+        serviceCollection.AddScoped<IAuthService, AuthService>();
+        serviceCollection.AddScoped<IUserService, UserService>();
     }
 }
