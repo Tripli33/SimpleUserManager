6b2d272f5741428fd75d35f707328460caaf8104
Roman Savenko <rsaven.wk@gmail.com>
2024-09-17 16:31:09 +0300

    feat: add user repo

---
+++ SimpleUserManager/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using SimpleUserManager.Models;
+using System.Linq.Expressions;
+
+namespace SimpleUserManager.Repositories;
+
+public interface IUserRepository
+{
+    User? GetById(int id);
+    User? GetByEmail(string email);
+    IEnumerable<User> GetAll();
+    IQueryable<User> GetAll(Expression<Func<User, bool>> filter);
+    Task CreateAsync(User user);
+    Task DeleteAsync(User user);
+    Task UpdateAsync(User user);
+}
---
+++ SimpleUserManager/Repositories/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using SimpleUserManager.DataContext;
+using SimpleUserManager.Models;
+using System.Linq.Expressions;
+
+namespace SimpleUserManager.Repositories;
+
+public class UserRepository(UserManagerDbContext context) : IUserRepository
+{
+    public async Task CreateAsync(User user)
+    {
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(User user)
+    {
+        context.Remove(user);
+        await context.SaveChangesAsync();
+    }
+
+    public IEnumerable<User> GetAll() => context.Users;
+
+    public IQueryable<User> GetAll(Expression<Func<User, bool>> filter) => context.Users.Where(filter);    
+
+    public User? GetByEmail(string email) => context.Users.SingleOrDefault(u => u.Email == email);
+
+    public User? GetById(int id) => context.Users.SingleOrDefault(u => u.UserId == id);
+
+    public async Task UpdateAsync(User user)
+    {
+        context.Update(user);
+        await context.SaveChangesAsync();
+    }
+}
