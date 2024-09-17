9edc3b9285bd52e15d30c9345b4e4a0385e71ac9
Roman Savenko <rsaven.wk@gmail.com>
2024-09-17 16:33:36 +0300

    feat: add automapper profile

---
+++ SimpleUserManager/Utilities/AutoMapperProfiles/SimpleUserManagerAutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using SimpleUserManager.DTOs;
+using SimpleUserManager.Models;
+
+namespace SimpleUserManager.Utilities.AutoMapperProfiles
+{
+    public class SimpleUserManagerAutoMapperProfile : Profile
+    {
+        public SimpleUserManagerAutoMapperProfile()
+        {
+            CreateMap<User, UserManipulationDto>();
+            CreateMap<UserRegistrationDto, User>();
+        }
+    }
+}
